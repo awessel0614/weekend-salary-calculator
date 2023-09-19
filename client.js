5cb610f9e50e9d3da85c1966d977f1b805af2404
Ashley Wessel <amerino.dance@gmail.com>
2023-09-19 16:29:27 -0500

    now have employee list appending to the DOM

--- client.js
+++ client.js
@@ -2,3 +2,30 @@
 
 
 console.log('Hello, world!');
+
+
+function addEmployee(event){
+    event.preventDefault();
+
+
+    let firstName = document.querySelector(`#first-name`).value;
+    let lastName = document.querySelector(`#last-name`).value;
+    let idNumber = document.querySelector(`#id-number`).value;
+    let jobTitle = document.querySelector(`#job-title`).value;
+    let annualSalary = document.querySelector(`#annual-salary`).value;
+
+    let employeeList = document.querySelector(`#employee-list`);
+
+    employeeList.innerHTML += `
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSalary}</td>
+        </tr>
+    
+    
+    `;
+
+}
